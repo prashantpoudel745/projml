b9c273fc6a75d84ac3a47fec6b1fd2b83ced72e5
prashantpoudel745 <prashantpoudel745@gmail.com>
2024-12-26 23:30:04 +0545

    data transformed

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -59,3 +59,42 @@ class DataTransformation:
             return preprocessor
         except Exception as e:
             raise CustomException(e, sys)
+
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+            logging.info("Data loaded successfully")
+            preprocessing_obj = self.get_data_transformer_object()
+            target_column_name = "math_score"
+            numerical_columns = ["writing_score", "reading_score"]
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+            logging.info(f"Applying preprocessing on taining and testing dataset")
+            input_feature_train_arr = preprocessing_obj.fit_transform(
+                input_feature_train_df
+            )
+            input_feature_test_arr = preprocessing_obj.fit_transform(
+                input_feature_test_df
+            )
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[
+                input_feature_test_arr, np.array(target_feature_test_df)
+            ]  # np-c : This is a shorthand in NumPy to concatenate arrays column-wise. It stacks the columns of the input arrays side by side. Equivalent to np.concatenate((array1, array2), axis=1).
+            logging.info(f"Saved preprocessing object")
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj,
+            )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except:
+            pass
